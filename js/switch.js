dfd4f19f8d33a89dc707037756fb83b127c94a5d
Sven Friedemann <sven@erstellbar.de>
2016-05-20 11:20:25 +0200

    support multible switches

--- js/switch.js
+++ js/switch.js
@@ -5,14 +5,21 @@
  * @licence MIT
  *
  */
-(function (document, window) {
+(function () {
     'use strict';
     
-    var switchEl = document.querySelector('.switch');
-    switchEl.addEventListener('click', function(e) {
-        e.preventDefault();
-        switchEl.classList.toggle('is-active');
-        switchEl.classList.add('is-loading');
-        window.location = switchEl.querySelector('a.not-active').getAttribute('href');
-    })
-})(document, window);
+    var switchEls = document.querySelectorAll('.switch');
+
+    for (var i = 0; i < switchEls.length; ++i) {
+        (function() {
+            var switchEl = switchEls[i];
+
+            switchEls[i].addEventListener('click', function(e) {
+                e.preventDefault();
+                switchEl.classList.toggle('is-active');
+                switchEl.classList.add('is-loading');
+                window.location = switchEl.querySelector('a.not-active').getAttribute('href');
+            })
+        })();
+    }
+})();
